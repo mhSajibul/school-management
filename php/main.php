d938e1fef7deb9b1eee20ee8eb2ddae571df5818
Sojibul Islam <mh.sajibul@gmail.com>
2024-10-22 05:25:54 +0600

    add some

---
+++ php/main.php
@@ -0,0 +1,14 @@
+<?php
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+
+$sutdentID = $_POST['sutdentID'];
+$Bangla = $_POST['BanglaMark'];
+$English = $_POST['EnglishMark'];
+$Math = $_POST['MathMark'];
+$Science = $_POST['ScienceMark'];
+$Relegion = $_POST['RelegionMark'];
+
+echo "$sutdentID";
+?>
